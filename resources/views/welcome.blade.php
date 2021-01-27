0d88e5e375cb0447b2015462ca743e27ae47e431
ocordova <ocordova@fadq.org>
2021-01-27 18:56:16 +0100

    nuevo cambos del welcome dos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,6 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://vapor.laravel.com">otras lineas de textos para otro camhios </a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
