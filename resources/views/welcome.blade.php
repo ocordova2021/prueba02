c928fdd8fd0db5df51699663be6573bd04beca02
ocordova <ocordova@fadq.org>
2021-01-28 17:29:06 +0100

    modificamos la paguina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,8 +92,11 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://vapor.laravel.com">otro botones</a>
+                 
                     <a href="https://vapor.laravel.com">otras lineas de textos para otro camhios </a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                       <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://vapor.laravel.com">Vapor</a><a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
